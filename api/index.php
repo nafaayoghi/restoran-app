596b5d7d370b4cab15a448cd8959795515205171
nafaayoghi <nafaayoghi@gmail.com>
2026-08-25 19:48:29 +0700

    Switch session driver to array and enable verbose PHP errors

--- api/index.php
+++ api/index.php
@@ -1,36 +1,30 @@
 <?php
 
+// Aktifkan error reporting penuh agar tidak HTTP 500 polos
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-// Pastikan semua env penting terpasang sebelum framework dimuat
-$envs = [
-    'APP_ENV' => 'production',
-    'APP_DEBUG' => 'true',
-    'LOG_CHANNEL' => 'stderr',
-    'CACHE_STORE' => 'array',
-    'CACHE_DRIVER' => 'array',
-    'SESSION_DRIVER' => 'cookie',
-    'MAINTENANCE_DRIVER' => 'cache',
-    'MAINTENANCE_STORE' => 'array',
-    'DB_CONNECTION' => 'sqlite',
-    'DB_DATABASE' => '/tmp/database.sqlite',
-    'VIEW_COMPILED_PATH' => '/tmp/views',
-    'APP_CONFIG_CACHE' => '/tmp/bootstrap/cache/config.php',
-    'APP_SERVICES_CACHE' => '/tmp/bootstrap/cache/services.php',
-    'APP_PACKAGES_CACHE' => '/tmp/bootstrap/cache/packages.php',
-    'APP_ROUTES_CACHE' => '/tmp/bootstrap/cache/routes.php',
-    'APP_EVENTS_CACHE' => '/tmp/bootstrap/cache/events.php',
-];
+// Set storage dan cache path ke /tmp
+putenv('APP_STORAGE=/tmp/storage');
+putenv('VIEW_COMPILED_PATH=/tmp/views');
+putenv('LOG_CHANNEL=stderr');
+putenv('SESSION_DRIVER=array');
+putenv('CACHE_STORE=array');
+putenv('CACHE_DRIVER=array');
+putenv('MAINTENANCE_DRIVER=cache');
+putenv('MAINTENANCE_STORE=array');
 
-foreach ($envs as $key => $val) {
-    putenv("{$key}={$val}");
-    $_ENV[$key] = $val;
-    $_SERVER[$key] = $val;
-}
+$_ENV['APP_STORAGE'] = '/tmp/storage';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/views';
+$_ENV['LOG_CHANNEL'] = 'stderr';
+$_ENV['SESSION_DRIVER'] = 'array';
+$_ENV['CACHE_STORE'] = 'array';
+$_ENV['CACHE_DRIVER'] = 'array';
+$_ENV['MAINTENANCE_DRIVER'] = 'cache';
+$_ENV['MAINTENANCE_STORE'] = 'array';
 
-// Buat direktori runtime sementara
+// Buat direktori sementara yang dibutuhkan Laravel
 $dirs = [
     '/tmp/storage',
     '/tmp/storage/app',
@@ -50,8 +44,5 @@ foreach ($dirs as $dir) {
     }
 }
 
-if (!file_exists('/tmp/database.sqlite')) {
-    @touch('/tmp/database.sqlite');
-}
-
+// Jalankan aplikasi Laravel
 require __DIR__ . '/../public/index.php';
